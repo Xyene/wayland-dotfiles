b0b769b1fa81bc4fd03b81bdc3a6853ea7ce2712
Tudor Brindus <me@tbrindus.ca>
2020-03-18 20:14:30 -0400

    lock: fork to background

--- .scripts/blur-lock-multimonitor.sh
+++ .scripts/blur-lock-multimonitor.sh
@@ -16,6 +16,6 @@ do
     images="${images} ${image}"
 done
 
-swaylock --text-color=ffffff00 --inside-color=ffffff1c --ring-color=ffffff3e --line-color=ffffff00 --key-hl-color=00000080 --ring-ver-color=00000000 --inside-ver-color=0000001c --ring-wrong-color=00000055 --inside-wrong-color=0000001c $lock_args
+swaylock -f --text-color=ffffff00 --inside-color=ffffff1c --ring-color=ffffff3e --line-color=ffffff00 --key-hl-color=00000080 --ring-ver-color=00000000 --inside-ver-color=0000001c --ring-wrong-color=00000055 --inside-wrong-color=0000001c $lock_args
 
 rm $images
